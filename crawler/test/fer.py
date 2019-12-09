1c750c19767cde89b1109c0c1a1bc42e75212368
nelson0423 <46638300+nelson0423@users.noreply.github.com>
2019-12-09 14:41:05 +0800

    Foreign Exchange Rates

--- crawler/test/fer.py
+++ crawler/test/fer.py
@@ -1,5 +1,5 @@
 """
-痞客邦
+Foreign Exchange Rates
 """
 from crawler import logger
 import requests
@@ -12,35 +12,28 @@ warnings.filterwarnings("ignore")
 
 
 def process(url):
-    ret = dict()
-    ret["text_content"] = ""
     response = requests.get(url)
     response.encoding = "utf-8"  # 解決亂碼問題
     html = BeautifulSoup(response.text)
     # logger.debug(html)
     fer = html.select_one("table[title^='Foreign Exchange Rates']")
     # print(fer)
-    currMonth = fer.select("thead > tr > th")[2].text.strip()
+    currMonth = fer.select_one("thead > tr > th:nth-child(3)").text.strip()
     print("currMonth = {}".format(currMonth))
-    currMonthAry = [data for data in currMonth.split(" ") if data != ""]
-    currMonthAry.sort(reverse=False)
+    currMonthAry = [data for data in currMonth.split(" ") if data != ""][::-1]
     currMonthAry[1] = format({v: k for k, v in enumerate(calendar.month_name)}[currMonthAry[1]], "02d")
     currMonthStr = "-".join(currMonthAry)
     print("currMonthStr = {}".format(currMonthStr))
-    countryEleList = fer.select("tbody > tr > th[class!='subhead1']")
-    countryList = [ele.text.strip() for ele in countryEleList]
+    countryList = [ele.text.strip() for ele in fer.select("tbody > tr > th[class!='subhead1']")]
     print("len = {}, countryList = {}".format(len(countryList), countryList))
-    currencyEleList = fer.select("tbody > tr > td:nth-child(2)")
-    currencyList = [ele.text.strip() for ele in currencyEleList]
+    currencyList = [ele.text.strip() for ele in fer.select("tbody > tr > td:nth-child(2)")]
     print("len = {}, currencyList = {}".format(len(currencyList), currencyList))
-    erEleList = fer.select("tbody > tr > td:nth-child(3)")
-    erList = [ele.text.strip() for ele in erEleList]
+    erList = [ele.text.strip() for ele in fer.select("tbody > tr > td:nth-child(3)")]
     print("len = {}, erList = {}".format(len(erList), erList))
     df = pd.DataFrame(list(zip(countryList, currencyList, erList)),
                       columns=["country", "currency", "exchange_rate"])
     print(df)
-    df.to_csv("D:/Users/nelson_chou/Desktop/FER_" + currMonthStr + ".csv", sep=',', encoding='utf-8')
-    return ret
+    df.to_csv("D:/Users/nelson_chou/Desktop/FER_" + currMonthStr + ".csv", sep=",", encoding="utf-8")
 
 
 if __name__ == '__main__':
